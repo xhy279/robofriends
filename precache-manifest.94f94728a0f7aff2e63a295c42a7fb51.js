019c5ce293828920941441db11307d5d5cd83f74
hxia200 <hxia3@ncsu.edu>
2019-07-15 01:36:21 -0700

    Updates

---
+++ precache-manifest.94f94728a0f7aff2e63a295c42a7fb51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e8e88401a4b372e2ebbc51dcca7fd6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8ad6ee4d68fd23654738",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "fb10355d7dbce2b31836",
+    "url": "/robofriends/static/css/main.697ea4af.chunk.css"
+  },
+  {
+    "revision": "8ad6ee4d68fd23654738",
+    "url": "/robofriends/static/js/2.b9e47281.chunk.js"
+  },
+  {
+    "revision": "fb10355d7dbce2b31836",
+    "url": "/robofriends/static/js/main.0d0a66d7.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
